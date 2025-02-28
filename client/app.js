6867695957f0a96baecfe78b677544f21d3e9cf7
LUZAmaxxp <ayman.allouch@e-polytechnique.ma>
2025-02-28 10:44:16 +0000

    first commit

--- client/app.js
+++ client/app.js
@@ -696,7 +696,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       .then((data) => {
         // Update navbar profile
         document.getElementById("nav-username").textContent = data.username;
-        document.getElementsByClassName("profile-image").src = data.profilePic;
+        document.getElementsByClassName(".profile-image").src = data.profilePic;
       })
       .catch((error) => {
         console.error("Error loading profile:", error);
